8bf77be0cd8b60099f5543c0d30a6995cff48e45
Peter Bortas <zino@lysator.liu.se>
2002-05-04 01:07:27 +0200

    Some improvements.

--- client/client.sh
+++ client/client.sh
@@ -43,11 +43,11 @@ echo $$ > $pidfile
 
 if [ ! -x put ]; then
     make put
-fi
-if [ ! -x put ]; then
-    echo "FATAL: No put command found."
-    rm $pidfile
-    exit 2
+    if [ ! -x put ]; then
+        echo "FATAL: No put command found."
+        rm $pidfile
+        exit 2
+    fi
 fi
 
 grep -v \# projects.conf | ( while 
@@ -64,28 +64,43 @@ grep -v \# projects.conf | ( while
     fi
 
     (cd "$dir" &&
+     uncompressed=0
      NEWCHECK="`ls -l snapshot.tar.gz`";
      wget --dot-style=binary -N "$geturl" &&
      if [ X"`ls -l snapshot.tar.gz`" == X"$NEWCHECK" ]; then
         echo "NOTE: No newer snapshot for $project available. Skipping."
-        exit 0
      fi
-     rm -rf buildtmp && mkdir buildtmp && cd buildtmp &&
-     gzip -cd ../snapshot.tar.gz | tar xvf - &&
-     cd */. &&
+     rm -rf buildtmp && mkdir buildtmp && 
+     cd buildtmp &&
      for target in `echo $targets`; do
-        resultdir="../result_$target"
-        rm -rf "$resultdir" && mkdir "$resultdir" &&
-        cp export.stamp "$resultdir/" &&
-        make $target 2>&1> "$resultdir/RESULT";
-        if [ -f autobuild_result.tar.gz ]; then
-            mv autobuild_result.tar.gz "$resultdir/"
+        if [ \! -f "../last_$target" ] ||
+           [ "../last_$target" -ot ../../snapshot.tar.gz ]; then
+            if [ x"$uncompressed" = x0 ] ; then
+              echo "Uncompressing archive..." &&
+              (gzip -cd ../snapshot.tar.gz | tar xf -)
+              echo "done"
+              uncompressed=1
+            fi
+            cd */. 
+            resultdir="../../result_$target"
+            rm -rf "$resultdir" && mkdir "$resultdir" &&
+            cp export.stamp "$resultdir/" &&
+            echo "Building $target" &&
+            make $target 2>&1> "$resultdir/RESULT";
+            if [ -f autobuild_result.tar.gz ]; then
+                mv autobuild_result.tar.gz "$resultdir/"
+            else
+                (cd "$resultdir" && 
+                tar cvf autobuild_result.tar RESULT export.stamp |
+                gzip autobuild_result.tar)
+            fi
+            touch "../../last_$target";
+            ../../../put "$puturl" < "$resultdir/autobuild_result.tar.gz" &
+            cd ..
         else
-            (cd "$resultdir" && 
-             tar cvf autobuild_result.tar RESULT export.stamp |
-             gzip autobuild_result.tar)
+            echo "NOTE: Already built $project: $target. Skipping."
         fi
-        ./put "$puturl" < "$resultdir/autobuild_result.tar.gz" &
      done )
 done )
-     
+
+rm $pidfile
