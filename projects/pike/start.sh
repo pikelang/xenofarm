63cfbd2b85ceb992eeecfbf76c4cb14e5e68ec59
Peter Bortas <zino@lysator.liu.se>
2002-09-14 23:03:38 +0200

    Tentatively working process detection.

--- projects/pike/start.sh
+++ projects/pike/start.sh
@@ -4,12 +4,13 @@
 # pike xenofarm server starter
 #
 # Written by Peter Bortas, Copyright 2002
-# $Id: start.sh,v 1.2 2002/09/05 16:07:49 zino Exp $
+# $Id: start.sh,v 1.3 2002/09/14 21:03:38 zino Exp $
 # Licence: <Nilsson will insert Copyright of his choise here>
 ##############################################
 # Error codes:
 #  0: Exited without errors
 #  1: One or more of the servers was already running
+#  2: Failed to start one or more of the servers
 
 check_pidfile() {
     node=`uname -n`
@@ -27,25 +28,42 @@ check_pidfile() {
 }
 
 setup_pidfile() {
-    last_job=`ps | grep -v PID | sort | tail -1 | awk '{print $1}'`
-    echo $last_job > $pidfile
+    #Must compensate for all processes in the pipe, therefore tail -6
+    last_job="`ps | grep -v PID | tail -6 | head -1`"
+    jobname=`basename $pidfile | sed 's/\.pid//'`
+    #Check that it wasn't the last processes PID we got
+    if [ X"`echo $last_job | awk '{print $4}'`" != Xpike -o \
+         X"`echo $last_job | awk '{print $1}'`" = X$last_pid ] ; then
+        echo "Failed to start $jobname."
+        exit 2
+    else
+        last_pid=`echo $last_job | awk '{print $1}'`
+        echo "Started $jobname with PID $last_pid."
+        echo $last_pid > $pidfile
+    fi
 }
+
+error=0
 
 (check_pidfile gc
  pike gc.pike &
+ sleep 1 #Give it some time to fail
  setup_pidfile)
-error=$?
+last=$?
+if [ $last -gt $error ] ; then error=$last ; fi
 
 (check_pidfile server
  pike server.pike --verbose > log_server &
+ sleep 1 #Give it some time to fail
  setup_pidfile)
 last=$?
-if [ $error = 0 ] ; then error=$last ; fi
+if [ $last -gt $error ] ; then error=$last ; fi
 
 (check_pidfile result_parser
  pike result_parser.pike --verbose > log_result &
+ sleep 1 #Give it some time to fail
  setup_pidfile)
 last=$?
-if [ $error = 0 ] ; then error=$last ; fi
+if [ $last -gt $error ] ; then error=$last ; fi
 
-exit $last
+exit $error
