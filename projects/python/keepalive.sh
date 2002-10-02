86aa15cc2046257e437ab95d2051fbb9162f9c0d
Anders Qvist <quest@lysator.liu.se>
2002-10-02 15:05:04 +0200

    Some changes to actually make the script work.

--- projects/python/keepalive.sh
+++ projects/python/keepalive.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 MAILADDR="sfarmer@lysator.liu.se"
-XENOUSER="quest"
-#XENOUSER="sfarmer"
+XENOUSER="sfarmer"
 
-function check_alive {
-	if ! ps -fu $XENOUSER | grep $1 | grep -v grep > /dev/null; then
+check_alive () {
+	if ps -fu $XENOUSER | grep $1 | grep -v grep > /dev/null
+        then
+		echo > /dev/null
+	else
 		tail -30 $3 | mail $MAILADDR
-		echo "[`date '+%Y-%m-%d %H:%M:%S'`] keepalive.sh restarted $1" > $3
-		pike $1 $2 > $3 2>&1 &
+		echo "[`date '+%Y-%m-%d %H:%M:%S'`] keepalive.sh restarted $1" >> $3
+		pike $1 $2 >> $3 2>&1 &
 	fi
 }
 
