5b031e79ee270c7a9d0878a020b000188884a68e
Martin Nilsson <mani@lysator.liu.se>
2002-12-05 15:24:37 +0100

    Added defaults to enums and added binary to strings.

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 #
 # Database tables for Xenofarm.
 # Implemented to work with MySQL
-# $Id: tables.sql,v 1.6 2002/11/30 05:29:22 mani Exp $
+# $Id: tables.sql,v 1.7 2002/12/05 14:24:37 mani Exp $
 #
 
 # The generic build table.
@@ -9,7 +9,7 @@
 CREATE TABLE build (
   id      INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   time    INT UNSIGNED NOT NULL,
-  export  ENUM('FAIL','WARN','PASS') NOT NULL
+  export  ENUM('FAIL','WARN','PASS') NOT NULL DEFAULT 'FAIL'
 );
 
 
@@ -17,12 +17,12 @@ CREATE TABLE build (
 
 CREATE TABLE system (
   id        INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name      VARCHAR(255) NOT NULL,
-  sysname   VARCHAR(255) NOT NULL,
-  release   VARCHAR(255) NOT NULL,
-  version   VARCHAR(255) NOT NULL,
-  machine   VARCHAR(255) NOT NULL,
-  testname  VARCHAR(255) NOT NULL
+  name      VARCHAR(255) BINARY NOT NULL,
+  sysname   VARCHAR(255) BINARY NOT NULL,
+  release   VARCHAR(255) BINARY NOT NULL,
+  version   VARCHAR(255) BINARY NOT NULL,
+  machine   VARCHAR(255) BINARY NOT NULL,
+  testname  VARCHAR(255) BINARY NOT NULL
 );
 
 
@@ -35,7 +35,7 @@ CREATE TABLE task (
   id          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   sort_order  INT UNSIGNED NOT NULL,
   parent      INT UNSIGNED NOT NULL,
-  name        VARCHAR(255) NOT NULL
+  name        VARCHAR(255) BINARY NOT NULL
 );
 
 
@@ -49,7 +49,7 @@ CREATE TABLE task_result (
   build       INT UNSIGNED NOT NULL,
   system      INT UNSIGNED NOT NULL,
   task        INT UNSIGNED NOT NULL,
-  status      ENUM('FAIL','WARN','PASS') NOT NULL,
+  status      ENUM('FAIL','WARN','PASS') NOT NULL DEFAULT 'FAIL',
   warnings    INT UNSIGNED NOT NULL,
   time_spent  INT UNSIGNED NOT NULL,
   PRIMARY KEY (build, system, task)
