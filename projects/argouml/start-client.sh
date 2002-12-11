c02c4c8d445211337f550c36b0da6076272c16e2
Linus Tolke <linus@lysator.liu.se>
2002-12-11 22:24:42 +0100

    Save the output into a file so that the cronjob will not deliver mails.

--- projects/argouml/start-client.sh
+++ projects/argouml/start-client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Set JAVA_HOMEs.
+# Set JAVA_HOMEs. This list is for stanly.
 JAVA_HOME_1_2=${JAVA_HOME_1_2-/sw/jdk/jdk1.2}
 JAVA_HOME_1_3=${JAVA_HOME_1_3-/sw/jdk/j2sdk1_3_0_02}
 export JAVA_HOME_1_2 JAVA_HOME_1_3
@@ -8,4 +8,6 @@ export JAVA_HOME_1_2 JAVA_HOME_1_3
 ANT_OPTS=${ANT_OPTS--Xmx512M}
 export ANT_OPTS
 
-cd ../../client && ./client.sh --nolimit --configdir=../projects/argouml
+cd ../../client
+exec ./client.sh --nolimit --configdir=../projects/argouml > start-client.log
+
