21a776e82520862b671a154d8dab6af4cfc4c2fd
Niels Möller <nisse@lysator.liu.se>
2003-02-17 21:10:12 +0100

    Disabled make dist-check, it tends to leave write-protected directories around on the clients. Adapted the environment for running on lenin.

--- projects/lsh/source-transform.sh
+++ projects/lsh/source-transform.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+PATH=/i/autoconf/2.57/bin:/usr/local/bin:/usr/bin:/bin:/usr/ccs/bin
+export PATH
+
+# Don't know why gcc doesn't search /usr/local/lib by default. Using
+# LD_LIBRARY_PATH seems easier than passing flags to configure.
+
+LD_LIBRARY_PATH=/usr/local/lib
+
 project=$1
 result=$2
 stamp=$3
@@ -10,10 +18,12 @@ set -e
 
 rm -rf workdir dist
 cp -a $project workdir
-(cd workdir && misc/bootstrap) # Run automake, autoconf etc
-(cd workdir && ./configure -C) # Create makefiles
+(cd workdir && misc/bootstrap.sh) # Run automake, autoconf etc
+(cd workdir && ./configure -C \
+               --with-lib-path=/usr/local/lib \
+	       --with-include-path=/usr/local/include ) # Create makefiles
 (cd workdir && make bootstrap) # Generate source files
-(cd workdir && misc/make-dist) # Create lsh-xx.tar.gz
+(cd workdir && make dist)      # Create lsh-xx.tar.gz
 
 mkdir dist
 mv workdir/lsh-*.tar.gz dist/
@@ -22,6 +32,6 @@ echo $stamp > dist/buildid.txt
 cp workdir/misc/xenofarm.sh dist/create-response.sh
 chmod +x dist/create-response.sh
 
-tar cf $result.tar dist
-gzip $result.tar
+tar cf ./$result.tar dist
+gzip ./$result.tar
 exit 0
